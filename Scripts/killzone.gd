60f9187e1b35e1d2fe6de9e9f6b375c6e5007368
LEGOKOALA <144147004+LEGOKOALA@users.noreply.github.com>
2025-12-30 13:12:21 -0500

    Player movement update

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,4 +1,10 @@
 extends Area2D
 
-func _on_area_entered(area: Area2D) -> void:
-	pass
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(body: Node2D) -> void:
+	print("You Died")
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
