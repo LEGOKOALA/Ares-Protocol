dbce570cc4021e26d86ffe54d9991aa76a1dc144
MI7xZY <144147004+LEGOKOALA@users.noreply.github.com>
2026-01-02 18:26:21 -0500

    Player script update

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 @export var speed := 100
+@export var max_health := 5
+
+var current_health := max_health
 
 func _physics_process(delta: float) -> void:
 	var input_direction := Vector2.ZERO
@@ -10,3 +13,16 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = input_direction.normalized() * speed
 	move_and_slide()
+
+
+func take_damage(amount: int) -> void:
+	current_health -= amount
+	print("Player Health: ", current_health)
+	
+	if current_health <= 0:
+		die()
+
+		
+func die() -> void:
+	print("Player died")
+	get_tree().reload_current_scene()
